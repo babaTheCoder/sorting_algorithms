43532b8a64a9762728c51e429dc163651e256477
babaTheCoder <131319600+babaTheCoder@users.noreply.github.com>
2023-12-21 04:47:33 +0000

    3. Quick sort

--- sort.h
+++ sort.h
@@ -23,4 +23,8 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void swap(int *array, ssize_t item1, ssize_t item2);
+int lomuto_partition(int *array, ssize_t first, ssize_t last, size_t size);
+void qs(int *array, ssize_t first, ssize_t last, int size);
+void quick_sort(int *array, size_t size);
 #endif
